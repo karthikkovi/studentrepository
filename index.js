d6699abb82d109a20ff23a67ef34861a334d26a7
karthikkovi <karthi.kovi@gmail.com>
2020-08-09 19:04:37 +0930

    added authenticationn and logic to identify it is prod

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
+
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey],
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'changed something' });
-});
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(`App started on ${PORT}`);
